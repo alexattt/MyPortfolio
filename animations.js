d36829a6341a9b8c82da4299addf69b1cbbc1dca
alexattt <49170656+alexattt@users.noreply.github.com>
2021-01-31 20:04:34 +0200

    first commit

---
+++ animations.js
@@ -0,0 +1,84 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('progress', {
+  value: 100,
+  ease: 'none',
+  scrollTrigger: { 
+    trigger: "#mySection",
+    scrub: 0.3 
+  }
+});
+
+var tl = gsap.timeline();
+
+tl.from(".welcome-msg", {duration: 1, y: 200, opacity: 0})
+  .from(".desc", {duration: 1, y: 100, opacity: 0})
+  .from(".blue-logo", {duration: 1, y: 100, opacity: 0})
+  .from(".pink-logo", {duration: 0.6, x: 5, y: 5, opacity: 0})
+  .from(".white-logo", {duration: 0.6, x: 6, y: 5, opacity: 0})
+  .from(".scroll", {duration: 1, y: -100, opacity: 0});
+
+gsap.from(".pr", {
+  duration: 1,
+  y: 300,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page-two.content",
+    markers: true
+  }
+})
+
+gsap.from(".ptwo-title", {
+  duration: 1,
+  x: -100,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page-two.content",
+    markers: true
+  }
+})
+
+gsap.from(".about-photo", {
+  duration: 2,
+  y: -300,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page-three.content",
+    start: 'left+=500',
+    markers: true
+  }
+})
+
+gsap.from(".about-info", {
+  duration: 1,
+  x: 100,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page-three.content",
+    start: 'left+=500',
+    markers: true
+  }
+})
+
+gsap.from(".contact-form-column", {
+  duration: 1,
+  y: 200,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page-four.content",
+    start: 'left+=750',
+    markers: true
+  }
+})
+
+gsap.from(".logo-column", {
+  duration: 1,
+  x: 200,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: "#page-four.content",
+    start: 'left+=900',
+    markers: true
+  }
+})
+
---
+++ modal.js
@@ -0,0 +1,70 @@
+var s = skrollr.init();
+
+const mod_arr = [];
+const trigg_arr = [];
+const btn_arr = [];
+
+for (let i =1; i<7; i++) {
+  let modal = document.querySelector(`.modal${i}`);
+  mod_arr.push(modal);
+  let trigger = document.querySelector(`.trigger${i}`);
+  trigg_arr.push(trigger);
+  let closeButton = document.querySelector(`.close-button${i}`);
+  btn_arr.push(closeButton);
+}
+
+const togFunc_arr = [];
+
+function toggleModal1() {
+  mod_arr[0].classList.toggle("show-modal");
+}
+function toggleModal2() {
+  mod_arr[1].classList.toggle("show-modal");
+}
+function toggleModal3() {
+  mod_arr[2].classList.toggle("show-modal");
+}
+function toggleModal4() {
+  mod_arr[3].classList.toggle("show-modal");
+}
+function toggleModal5() {
+  mod_arr[4].classList.toggle("show-modal");
+}
+function toggleModal6() {
+  mod_arr[5].classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === mod_arr[0]) {
+        toggleModal1();
+    }
+    if (event.target === mod_arr[1]) {
+        toggleModal2();
+    }
+    if (event.target === mod_arr[2]) {
+        toggleModal3();
+    }
+    if (event.target === mod_arr[3]) {
+        toggleModal4();
+    }
+    if (event.target === mod_arr[4]) {
+        toggleModal5();
+    }
+    if (event.target === mod_arr[5]) {
+        toggleModal6();
+    }
+}
+
+trigg_arr[0].addEventListener("click", toggleModal1);
+trigg_arr[1].addEventListener("click", toggleModal2);
+trigg_arr[2].addEventListener("click", toggleModal3);
+trigg_arr[3].addEventListener("click", toggleModal4);
+trigg_arr[4].addEventListener("click", toggleModal5);
+trigg_arr[5].addEventListener("click", toggleModal6);
+btn_arr[0].addEventListener("click", toggleModal1);
+btn_arr[1].addEventListener("click", toggleModal2);
+btn_arr[2].addEventListener("click", toggleModal3);
+btn_arr[3].addEventListener("click", toggleModal4);
+btn_arr[4].addEventListener("click", toggleModal5);
+btn_arr[5].addEventListener("click", toggleModal6);
+window.addEventListener("click", windowOnClick); 
